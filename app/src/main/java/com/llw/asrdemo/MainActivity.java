2e3ea665720445d5320adb3fe42c67644956226b
llw <1317057860@qq.com>
2021-04-30 11:31:56 +0800

    修改一下

--- app/src/main/java/com/llw/asrdemo/MainActivity.java
+++ app/src/main/java/com/llw/asrdemo/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements EventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         initView();
         initPermission();
 
